5c7eb22fa8f1348953c348703d77dedb1594a6b0
aroncaglia <adrien01du2a@gmail.com>
2017-05-15 09:42:15 +0200

    Add files via upload
    nouv.version

--- untitled/Regi.php
+++ untitled/Regi.php
@@ -1,4 +1,11 @@
-<?php
-$age_du_visiteur = 17;
-echo $age_du_visiteur;
-?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Ma page web</title>
+</head>
+<body>
+<h1>Ma page web</h1>
+<p>Aujourd'hui nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+</body>
+</html>
--- untitled/deb.php
+++ untitled/deb.php
@@ -1,4 +1,5 @@
-<?php
-$age_du_visiteur = 17;
-echo $age_du_visiteur;
-?>
+<p>Bonjour !</p>
+
+<p>Je sais comment tu t'appelles, hé hé. Tu t'appelles <?php echo $_POST['pseudo']; ?> !</p>
+
+<p>Si tu veux changer de prénom, <a href="test5.html">clique ici</a> pour revenir à la page formulaire.php.</p>
